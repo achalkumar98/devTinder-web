4fbbbc6835c92d6ba949cea49f82b69ee88e2866
achalkumar98 <hackerachal1620@gmail.com>
2025-08-28 21:24:49 +0530

    Build the Chat and Install socket

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { createSocketConnection } from "../utils/socket";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
 
 const Chat = () => {
   const { targetUserId } = useParams();
@@ -14,6 +16,27 @@ const Chat = () => {
 
   const userId = user?._id;
 
+  const fetchChatMessages = async () => {
+    const chat = await axios.get(BASE_URL + "/api/chat/" + targetUserId, {
+      withCredentials: true,
+    });
+    console.log(chat.data.messages);
+
+    const chatMessages = chat?.data?.messages.map((msg) => {
+      const { senderId, text } = msg;
+      return {
+        firstName: senderId?.firstName,
+        lastName: senderId?.lastName,
+        text,
+      };
+    });
+    setMessages(chatMessages);
+  };
+
+  useEffect(() => {
+    fetchChatMessages();
+  }, []);
+
   useEffect(() => {
     if (!userId) {
       return;
@@ -25,9 +48,9 @@ const Chat = () => {
       targetUserId,
     });
 
-    socket.on("messageReceived", ({firstName, text}) => {
-        console.log(firstName + ":  " + text);
-        setMessages((messages) => [...messages, { firstName, text }]);
+    socket.on("messageReceived", ({ firstName, lastName, text }) => {
+      console.log(firstName + ":  " + text);
+      setMessages((messages) => [...messages, { firstName, lastName, text }]);
     });
 
     return () => {
@@ -39,6 +62,7 @@ const Chat = () => {
     const socket = createSocketConnection();
     socket.emit("sendMessage", {
       firstName: user.firstName,
+      lastName: user.lastName,
       userId,
       targetUserId,
       text: newMessage,
@@ -57,15 +81,23 @@ const Chat = () => {
       <div className="flex-1 overflow-y-auto p-5 space-y-4 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-transparent">
         {messages.map((msg, index) => {
           return (
-            <div key={index} className="chat chat-start">
-              <div className="chat-header text-sm font-medium text-gray-300">
-                {msg.firstName}
-                <time className="text-xs opacity-50 ml-2">2 hours ago</time>
+            <div
+              key={index}
+              className={`flex ${
+                user.firstName === msg.firstName
+                  ? "justify-end"
+                  : "justify-start"
+              }`}
+            >
+              <div className="max-w-xs">
+                <div className="text-sm font-medium text-gray-300">
+                  {`${msg.firstName} ${msg.lastName}`}
+                </div>
+                <div className="bg-slate-600 text-white px-4 py-2 rounded-2xl shadow-md">
+                  {msg.text}
+                </div>
+                <div className="text-xs text-gray-400 mt-1">Seen</div>
               </div>
-              <div className="chat-bubble bg-indigo-600 text-white shadow-md px-4 py-2 rounded-2xl">
-                {msg.text}
-              </div>
-              <div className="chat-footer text-xs text-gray-400 mt-1">Seen</div>
             </div>
           );
         })}
